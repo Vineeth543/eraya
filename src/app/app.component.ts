a458ff9de72c3e049a75e60dd78f533ecc4d22e3
Vineeth Serigar <vineeth.serigar@netcracker.com>
2025-01-06 13:07:06 +0530

    [refactor] clean up code style and add Prettier configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less',
 })
-export class AppComponent {
-  title = 'eraya';
-}
+export class AppComponent {}
