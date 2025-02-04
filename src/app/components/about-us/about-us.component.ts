cd60c9df9048c9dc2109081dc305a1540f0964e0
Vineeth <vineethserigar17@gmail.com>
2025-02-04 17:58:40 +0530

    [feat] update Home component, and enhance header navigation

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -3,7 +3,6 @@ import { HeaderComponent } from '../header/header.component';
 
 @Component({
     selector: 'app-about-us',
-    imports: [HeaderComponent],
     templateUrl: './about-us.component.html',
     styleUrl: './about-us.component.less',
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { CommonModule } from '@angular/common';
+
+export interface Service {
+    title: string;
+    icon: string;
+    description: string;
+}
 
 @Component({
     selector: 'app-home',
-    imports: [ButtonComponent],
+    imports: [CommonModule, ButtonComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.less',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    services: Service[] = [
+        {
+            title: 'Home Interior',
+            icon: 'home',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        },
+        {
+            title: 'Office Interior',
+            icon: 'office',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        },
+        {
+            title: 'Restaurant Interior',
+            icon: 'kitchen',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        },
+        {
+            title: 'Architecture',
+            icon: 'architecture',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        },
+    ];
+}
