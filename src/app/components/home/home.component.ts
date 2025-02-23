7ee83eee68fe64cfacc104e52b72a78df47ea3d7
Vineeth <vineethserigar17@gmail.com>
2025-02-24 00:52:50 +0530

    [feat] add services interface and data; update Home component to utilize services data and improve layout responsiveness

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,40 +1,47 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from '../../shared/button/button.component';
+import { Service } from '../../interfaces/services.interface';
+import { SERVICES } from '../../data/services.data';
 import { CommonModule } from '@angular/common';
-
-export interface Service {
-    title: string;
-    icon: string;
-    description: string;
-}
+import { ButtonComponent } from '../../shared/button/button.component';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-home',
     imports: [CommonModule, ButtonComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.less',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
-    services: Service[] = [
-        {
-            title: 'Home Interior',
-            icon: 'home',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        },
-        {
-            title: 'Office Interior',
-            icon: 'office',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        },
-        {
-            title: 'Restaurant Interior',
-            icon: 'kitchen',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        },
-        {
-            title: 'Architecture',
-            icon: 'architecture',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        },
-    ];
+export class HomeComponent implements AfterViewInit {
+    @ViewChild('cardContainer', { static: true }) private cardContainer!: ElementRef;
+
+    private readonly MAX_ROWS: number = 2;
+    private readonly CARD_GAP: number = 28;
+    private readonly CARD_WIDTH: number = 454;
+
+    public services: Service[] = SERVICES;
+    public visibleServices: Service[] = [];
+    public moreServicesCount: number = 0;
+    public cardContainerWidth: number = 0;
+
+    constructor(private readonly cdr: ChangeDetectorRef) {}
+
+    @HostListener('window:resize')
+    public onWindowResize(): void {
+        this.updateVisibleServices();
+    }
+
+    public ngAfterViewInit(): void {
+        this.updateVisibleServices();
+    }
+
+    private updateVisibleServices(): void {
+        const containerWidth: number = this.cardContainer.nativeElement.offsetWidth;
+        const cardsPerRow: number = Math.floor((containerWidth + this.CARD_GAP - this.CARD_WIDTH) / (this.CARD_WIDTH + this.CARD_GAP));
+        const maxCards: number = cardsPerRow * this.MAX_ROWS;
+        this.visibleServices = this.services.slice(0, maxCards);
+        this.moreServicesCount = this.services.length - maxCards;
+        this.cardContainerWidth = cardsPerRow * (this.CARD_WIDTH + this.CARD_GAP) - this.CARD_GAP;
+        this.cardContainerWidth < 0 && (this.cardContainerWidth = 0);
+        this.cdr.detectChanges();
+    }
 }
---
+++ src/app/data/services.data.ts
@@ -0,0 +1,49 @@
+import { Service } from '../interfaces/services.interface';
+
+export const SERVICES: Service[] = [
+    {
+        title: 'Home Interior',
+        icon: 'home',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Office Interior',
+        icon: 'office',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Restaurant Interior',
+        icon: 'kitchen',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Architecture',
+        icon: 'architecture',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Resort Interior',
+        icon: 'home',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Hospital Interior',
+        icon: 'office',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Institute Interior',
+        icon: 'kitchen',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Showroom Interior',
+        icon: 'architecture',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        title: 'Shop Interior',
+        icon: 'home',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+];
