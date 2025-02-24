26e0311ccc97138a5e6a82fb202d438c5985c32c
Vineeth <vineethserigar17@gmail.com>
2025-02-24 14:48:40 +0530

    [feat] update logo assets and adjust styles across components

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    imports: [RouterLink, RouterLinkActive],
+    imports: [CommonModule, RouterLink, RouterLinkActive],
     styleUrl: './header.component.less',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    public readonly links: { name: string; path: string }[] = [
+        { name: 'Home', path: '/home' },
+        { name: 'About', path: '/about' },
+        { name: 'Services', path: '/services' },
+        { name: 'Projects', path: '/projects' },
+        { name: 'Contact', path: '/contact' },
+    ];
+}
