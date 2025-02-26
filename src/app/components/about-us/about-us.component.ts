f71ca6ee1cb752b7a8db1cd4d242eb1af4267cc9
Vineeth <vineethserigar17@gmail.com>
2025-02-26 11:40:07 +0530

    [feat] add About Us component with dynamic content and new images

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { FloatingSquaresComponent } from '../../shared/floating-squares/floating-squares.component';
 
 @Component({
     selector: 'app-about-us',
     templateUrl: './about-us.component.html',
     styleUrl: './about-us.component.less',
+    imports: [CommonModule, FloatingSquaresComponent, ButtonComponent],
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+    public readonly leftStrengths: string[] = ['Flexible Time', 'Perfect Work', 'Client Priority'];
+    public readonly rightStrengths: string[] = ['Innovative Ideas', 'Quality Work', 'Fast Delivery'];
+}
