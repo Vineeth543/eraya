d6fc2c014a2c155b452d73ce4f9a351dc2ed30c3
Vineeth Serigar <vineeth.serigar@netcracker.com>
2025-05-11 12:47:37 +0530

    [update] update Instagram URL to point to the specific profile

--- src/app/data/socials.data.ts
+++ src/app/data/socials.data.ts
@@ -1,7 +1,7 @@
 import { Socials } from '../interfaces/socials.interface';
 
 export const SOCIALS: Socials[] = [
-    { title: 'Instagram', icon: 'instagram', url: 'https://www.instagram.com/' },
+    { title: 'Instagram', icon: 'instagram', url: 'https://www.instagram.com/eraya__interiors' },
     { title: 'LinkedIn', icon: 'linkedin', url: 'https://www.linkedin.com/' },
     { title: 'Facebook', icon: 'facebook', url: 'https://www.facebook.com/' },
     { title: 'X', icon: 'x', url: 'https://x.com/' },
